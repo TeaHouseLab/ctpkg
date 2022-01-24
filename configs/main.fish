c3e24b4e54ff96ea6716e757d5e3b4bc50d312a3
tsingkwai <tsingkwai@bluelaketable>
2022-01-24 10:57:50 +0800

    2022-01-24_02:57:50

--- configs/main.fish
+++ configs/main.fish
@@ -6,7 +6,7 @@ if [ "$ctpm_source" = "" ]
     set ctpm_source https://ctpm.ruzhtw.top/
 end
 set_color cyan
-echo "$prefix CenterLinux Package Manager Version pomelo@build11 | TeaHouseLab at ruzhtw.top"
+echo "$prefix CenterLinux Package Manager Version BlackDeath@build1 | TeaHouseLab at ruzhtw.top"
 set_color normal
 switch $argv[1]
     case c
@@ -64,7 +64,7 @@ switch $argv[1]
         uninstall_script ctpkg
     case v version
         set_color yellow
-        echo "pomelo@build11"
+        echo "BlackDeath@build1"
         set_color normal
     case h help '*'
         help_echo
